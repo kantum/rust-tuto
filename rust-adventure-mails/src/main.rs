ffb4f77d3ad6d88af3b38e6654b847db8d712048
kantum <quentin@durot.co>
2023-07-06 02:05:08 +0200

    feat: add rust adventure mail tutorial

---
+++ rust-adventure-mails/src/main.rs
@@ -0,0 +1,23 @@
+use dotenv::dotenv;
+
+fn main() {
+    dotenv().ok();
+    let api_token = std::env::var("API_TOKEN")
+          .expect("expected there to be an api token");
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("a successful request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    println!("{}",response);
+}
